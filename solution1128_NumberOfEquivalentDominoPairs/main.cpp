b7e8ace47035520baf4376eacbfee693c1954700
Cunjun Wang <13621691063@163.com>
2019-08-10 11:26:29 +0800

    1122 finishes

--- solution1128_NumberOfEquivalentDominoPairs/main.cpp
+++ solution1128_NumberOfEquivalentDominoPairs/main.cpp
@@ -2,22 +2,31 @@
 // Created by 王存俊 on 2019-08-09.
 //
 
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
 class Solution {
 public:
-    int numEquivDominoPairs(vector <vector<int>> &dominoes) {
+    int numEquivDominoPairs(vector<vector<int>> &dominoes) {
         int size = dominoes.size();
         if (size == 0) return 0;
         int innerSize = dominoes[0].size();
         if (innerSize == 0) return 0;
 
-        unordered_map map =
+        int count = 0;
+        unordered_map<int, int> map;
         for (int i = 0; i < size; i++) {
             vector<int> ith = dominoes[i];
-            int min = min(ith[0], ith[1]);
-            int max = max(ith[0], ith[1]);
-            int encode = min * 10 + max;
+            int encode = min(ith[0], ith[1]) * 10 + max(ith[0], ith[1]);
+            int value = map[encode];
+            count += value;
+            if (value != 0) {
+                map[encode] = value + 1;
+            } else {
+                map[encode] = 1;
+            }
         }
-
         return count;
     }
 };
