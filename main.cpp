eb7d7ea8adc7b67d3a9db893bd8982998047d1ae
Cunjun Wang <13621691063@163.com>
2019-08-11 16:24:28 +0800

    1013 finishes

--- main.cpp
+++ main.cpp
@@ -8,51 +8,31 @@ using namespace std;
 
 class Solution {
 public:
-    int largestSumAfterKNegations(vector<int> &A, int K) {
-        sort(A.begin(), A.end());
-        int negativeCount = 0;
-        for (int i : A) {
-            if (i < 0) {
-                negativeCount++;
-            }
-        }
+    bool canThreePartsEqualSum(vector<int> &A) {
         int sum = 0;
-        // 非正数数量比K大，全都取相反数
-        if (negativeCount >= K) {
-            for (int i = 0; i < K; i++) {
-                A[i] = -A[i];
-            }
-        } else {
-            for (int i = 0; i < negativeCount; i++) {
-                A[i] = -A[i];
-            }
-            K = K - negativeCount;
-            if (K % 2 == 1) {
-                int min = *min_element(A.begin(), A.end());
-                sum -= 2 * min;
-            }
-        }
         for (int i : A) {
             sum += i;
         }
-        return sum;
+        if (sum % 3 != 0)
+            return false;
+        int avg = sum / 3;
+        int curSum = 0;
+        int times = 0;
+        for (int i = 0; i < A.size(); i++) {
+            curSum += A[i];
+            if (curSum == avg) {
+                times++;
+                curSum = 0;
+            }
+        }
+        return times == 3;
     }
 };
 
 int main() {
-    vector<int> A = {4, 2, 3};
-    int K = 1;
-    cout << "Sum = " << Solution().largestSumAfterKNegations(A, K) << endl;
+    vector<int> A = {0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1};
+    cout << Solution().canThreePartsEqualSum(A) << endl;
 
-    A = {3, -1, 0, 2};
-    K = 3;
-    cout << "Sum = " << Solution().largestSumAfterKNegations(A, K) << endl;
-
-    A = {2, -3, -1, 5, -4};
-    K = 2;
-    cout << "Sum = " << Solution().largestSumAfterKNegations(A, K) << endl;
-
-    A = {-8, 3, -5, -3, -5, -2};
-    K = 6;
-    cout << "Sum = " << Solution().largestSumAfterKNegations(A, K) << endl;
+    A = {0, 2, 1, -6, 6, 7, 9, -1, 2, 0, 1};
+    cout << Solution().canThreePartsEqualSum(A) << endl;
 }
---
+++ solution1013_PartitionArrayIntoThreePartsWithEqualSum/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 王存俊 on 2019-08-11.
+//
+
+class Solution {
+public:
+    bool canThreePartsEqualSum(vector<int>& A) {
+
+    }
+};
