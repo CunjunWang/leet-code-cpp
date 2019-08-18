4af35a323ace173c6f121d7a5c55210f36a04f1c
Cunjun Wang <13621691063@163.com>
2019-08-18 08:25:32 -0400

    313 finishe

--- solution0313_SuperUglyNumber/main.cpp
+++ solution0313_SuperUglyNumber/main.cpp
@@ -1,10 +1,6 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <map>
-#include <stack>
-
-using namespace std;
+//
+// Created by 王存俊 on 2019-08-18.
+//
 
 class Solution {
 public:
@@ -29,12 +25,3 @@ public:
         return memo[n - 1];
     }
 };
-
-int main() {
-    int n = 12;
-    vector<int> primes = {2, 7, 13, 19};
-    cout << Solution().nthSuperUglyNumber(n, primes) << endl;
-//    for (int i = 1; i < n + 1; i++) {
-//        cout << Solution().nthSuperUglyNumber(i, primes) << endl;
-//    }
-}
