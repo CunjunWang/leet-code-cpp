e451e0aebdd7a70deed83fbc3d6848b8630870ac
Cunjun Wang <13621691063@163.com>
2019-08-20 17:24:04 -0400

    0392 finishes

--- solution0383_RansomNote/main.cpp
+++ solution0383_RansomNote/main.cpp
@@ -5,6 +5,20 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-
+        unordered_map<char, int> count;
+        for (int i = 0; i < magazine.size(); i++) {
+            char c =  magazine[i];
+            if (count[c] == 0) {
+                count[c] = 1;
+            } else {
+                count[c]++;
+            }
+        }
+        for (int i = 0; i < ransomNote.size(); i++) {
+            char c = ransomNote[i];
+            if (count[c] == 0) return false;
+            count[c]--;
+        }
+        return true;
     }
 };
