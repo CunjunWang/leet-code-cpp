9eb38f1c7b35397dae7ce4c7ff28e87db5fac2dc
Cunjun Wang <13621691063@163.com>
2019-08-24 13:45:48 -0400

    717 finishes

--- solution0717_1BitAnd2BitCharacters/main.cpp
+++ solution0717_1BitAnd2BitCharacters/main.cpp
@@ -1,16 +1,10 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <map>
-#include <stack>
-#include <sstream>
-#include "TreeNode.h"
-
-using namespace std;
+//
+// Created by 王存俊 on 2019-08-24.
+//
 
 class Solution {
 public:
-    bool isOneBitCharacter(vector<int> &bits) {
+    bool isOneBitCharacter(vector<int>& bits) {
         int countOne = 0;
         for (int i = 0; i < bits.size() - 1; i++) {
             if (bits[i] == 1) {
