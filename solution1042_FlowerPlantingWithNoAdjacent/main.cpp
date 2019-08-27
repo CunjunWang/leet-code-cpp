f0d1568fadf39115a4e2a96e64254b97736497f5
Cunjun Wang <13621691063@163.com>
2019-08-26 20:54:27 -0400

    022 finishes

--- solution1042_FlowerPlantingWithNoAdjacent/main.cpp
+++ solution1042_FlowerPlantingWithNoAdjacent/main.cpp
@@ -2,41 +2,86 @@
 // Created by 王存俊 on 2019-08-10.
 //
 
-// TODO:
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+#include <sstream>
+#include <cassert>
+#include <list>
+#include <set>
+#include "TreeNode.h"
+#include "ListNode.h"
+
+using namespace std;
+
+// TODO: TLE
 class Solution {
 public:
     vector<int> gardenNoAdj(int N, vector<vector<int>> &paths) {
+        vector<int> color(N, 1);
 
-        vector<int> res(N);
-        vector<vector<int>> G(N);
-        for (vector<int> &p : paths) {
-            G[p[0] - 1].push_back(p[1] - 1);
-            G[p[1] - 1].push_back(p[0] - 1);
-        }
-        for (int i = 0; i < N; ++i) {
-            int colors[5] = {};
-            for (int j : G[i])
-                colors[res[j]] = 1;
-            for (int c = 4; c > 0; --c)
-                if (!colors[c])
-                    res[i] = c;
-        }
-        return res;
+        if (N == 0)
+            return {};
 
-//        map<int, vector<int>> table;
-//        vector<int> answer;
-//        for (int i = 0; i < paths.size(); i++) {
-//            int node = paths[i][0];
-//            table[node].push_back(paths[i][1]);
-//        }
-//        vector<bool> used(4, false);
-//        for (int i = 0; i < N; i++) {
-//            for (int j = 0; j < 4; j++) {
-//                if (!used[j]) {
-//                    answer.push_back(j + 1);
-//                    used[j] = true;
-//                }
-//            }
-//        }
+        if (paths.empty())
+            return color;
+
+        // build a graph use adjacent matrix
+        vector<vector<bool>> g(N, vector<bool>(N, false));
+        for (int i = 0; i < paths.size(); i++) {
+            vector<int> path = paths[i];
+            g[path[0] - 1][path[1] - 1] = g[path[1] - 1][path[0] - 1] = true;
+        }
+
+
+
+        // bfs
+        set<int> starts;
+        for (int i = 0; i < paths.size(); i++) {
+            starts.insert(paths[i][0]);
+        }
+        queue<int> todo; // next index
+        for (auto iter = starts.begin(); iter != starts.end(); iter++) {
+            todo.push(*iter);
+        }
+
+        while (!todo.empty()) {
+            int cur = todo.front();
+            todo.pop();
+
+            vector<bool> available = {true, true, true, true};
+            for (int i = 0; i < g[cur - 1].size(); i++) {
+                // if connected
+                if (g[cur - 1][i]) {
+                    if (color[i] != -1) {
+                        available[color[i] - 1] = false;
+                    } else {
+                        todo.push(i + 1);
+                    }
+                }
+            }
+            for (int j = 0; j < 4; j++) {
+                if (available[j]) {
+                    color[cur - 1] = j + 1;
+                    break;
+                }
+            }
+        }
+
+        return color;
     }
 };
+
+int main() {
+    int N = 4;
+    vector<vector<int>> paths = {{1, 2},
+                                 {3, 4}};
+    vector<int> colors = Solution().gardenNoAdj(N, paths);
+    for (int i = 0; i < colors.size(); i++) {
+        cout << colors[i] << " ";
+    }
+
+}
