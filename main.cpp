52ccc9a72da5203bf587d0c4b012b8f8a3392f1d
Cunjun Wang <13621691063@163.com>
2019-08-27 18:29:48 -0400

    322 finishes

--- main.cpp
+++ main.cpp
@@ -15,55 +15,25 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> highFive(vector<vector<int>> &items) {
-        map<int, priority_queue<int>> record;
-        for (int i = 0; i < items.size(); i++) {
-            vector<int> pairScore = items[i];
-            int id = pairScore[0];
-            int score = pairScore[1];
-            record[id].push(score);
-        }
+    int coinChange(vector<int> &coins, int amount) {
+        if (amount == 0 || coins.empty())
+            return 0;
 
-        vector<vector<int>> res;
+        vector<int> memo(amount + 1, amount + 1);
+        memo[0] = 0;
 
-        for (auto it : record) {
-            vector<int> pairAvg;
-            pairAvg.push_back(it.first);
-            priority_queue<int> scores = it.second;
-            int size = scores.size();
-            int loop = min(size, 5);
-            int sum = 0;
-            for (int i = 0; i < loop; i++) {
-                sum += scores.top();
-                scores.pop();
+        for (int j = 0; j < coins.size(); j++) {
+            for (int i = coins[j]; i < amount + 1; i++) {
+                memo[i] = min(memo[i], 1 + memo[i - coins[j]]);
             }
-            int avg = sum / loop;
-            pairAvg.push_back(avg);
-
-            res.push_back(pairAvg);
         }
 
-        return res;
+        return memo[amount] == amount + 1 ? -1 : memo[amount];
     }
 };
 
 int main() {
-    vector<vector<int>> items = {{1, 91},
-                                 {1, 92},
-                                 {2, 93},
-                                 {2, 97},
-                                 {1, 60},
-                                 {2, 77},
-                                 {1, 65},
-                                 {1, 87},
-                                 {1, 100},
-                                 {2, 100},
-                                 {2, 76}};
-    vector<vector<int>> res = Solution().highFive(items);
-    for (int i = 0; i < res.size(); i++) {
-        for (int j = 0; j < res[0].size(); j++) {
-            cout << res[i][j] << " ";
-        }
-        cout << endl;
-    }
+    vector<int> coins = {2};
+    int amount = 3;
+    cout << Solution().coinChange(coins, amount) << endl;
 }
---
+++ solution0322_CoinChange/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 王存俊 on 2019-08-27.
+//
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+
+    }
+};
