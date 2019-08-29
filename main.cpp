cca9142d88d1467b1c1fdea6113efb0824c01574
Cunjun Wang <13621691063@163.com>
2019-08-29 10:17:11 -0400

    243 finishes

--- main.cpp
+++ main.cpp
@@ -15,14 +15,19 @@ using namespace std;
 
 class Solution {
 public:
-    int subarraySum(vector<int> &nums, int k) {
-        int res = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int sum = 0;
-            for (int j = i; j < nums.size(); j++) {
-                sum += nums[j];
-                if (sum == k)
-                    res++;
+    int shortestDistance(vector<string> &words, string word1, string word2) {
+        int size = words.size();
+        int mostRecentWord1Index = -1;
+        int mostRecentWord2Index = -1;
+        int res = size + 1;
+        for (int i = 0; i < size; i++) {
+            if (words[i] == word1) {
+                mostRecentWord1Index = i;
+            } else if (words[i] == word2) {
+                mostRecentWord2Index = i;
+            }
+            if (mostRecentWord1Index != -1 && mostRecentWord2Index != -1) {
+                res = min(res, abs(mostRecentWord1Index - mostRecentWord2Index));
             }
         }
         return res;
@@ -30,4 +35,7 @@ public:
 };
 
 int main() {
+    vector<string> words = {"practice", "makes", "perfect", "coding", "makes"};
+    string word1 = "coding", word2 = "practice";
+    cout << Solution().shortestDistance(words, word1, word2) << endl;
 }
--- solution0243_ShortestWordDistance/main.cpp
+++ solution0243_ShortestWordDistance/main.cpp
@@ -16,15 +16,30 @@
 #include "ListNode.h"
 
 using namespace std;
-// TODO:
+
 class Solution {
 public:
     int shortestDistance(vector<string> &words, string word1, string word2) {
-        int word1Index = find(words.begin(), words.end(), word1) - words.begin();
-        int word2Index = find(words.begin(), words.end(), word2) - words.begin();
-        return 0;
+        int size = words.size();
+        int mostRecentWord1Index = -1;
+        int mostRecentWord2Index = -1;
+        int res = size + 1;
+        for (int i = 0; i < size; i++) {
+            if (words[i] == word1) {
+                mostRecentWord1Index = i;
+            } else if (words[i] == word2) {
+                mostRecentWord2Index = i;
+            }
+            if (mostRecentWord1Index != -1 && mostRecentWord2Index != -1) {
+                res = min(res, abs(mostRecentWord1Index - mostRecentWord2Index));
+            }
+        }
+        return res;
     }
 };
 
 int main() {
+    vector<string> words = {"practice", "makes", "perfect", "coding", "makes"};
+    string word1 = "coding", word2 = "practice";
+    cout << Solution().shortestDistance(words, word1, word2) << endl;
 }
