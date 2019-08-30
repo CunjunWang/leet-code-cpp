28b7f4e6dc6823cd5d004d7ad6d2d9fa13317794
Cunjun Wang <13621691063@163.com>
2019-08-30 11:51:38 -0400

    15 finishes

--- main.cpp
+++ main.cpp
@@ -15,53 +15,56 @@ using namespace std;
 
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        // dp[i][j]: s(i...j) is palindrome
-        int size = s.size();
-        if (size <= 1)
-            return s;
+    vector<vector<int>> threeSum(vector<int> &nums) {
+        vector<vector<int>> res;
+        int size = nums.size();
+        if (size <= 2)
+            return res;
 
-        vector<vector<bool>> dp(size, vector<bool>(size, false));
-        int maxLen = 1, start = -1, end = -1;
-        string palindrome;
+        sort(nums.begin(), nums.end());
+        if (nums[0] > 0 || nums[size - 1] < 0)
+            return res;
 
-        // if length == 1
-        for (int i = 0; i < size; i++) {
-            dp[i][i] = true;
-            palindrome = s.substr(i, 1);
-        }
+        for (int i = 0; i < size - 2; i++) {
+            // 剪枝, 因为此时array已经sort过了,
+            // 如果nums[i]都已经非负了, 一定不可能3个数和为0
+            if (nums[i] > 0)
+                break;
 
-        // if length == 2
-        for (int i = 0; i < size - 1; i++) {
-            if (s[i] == s[i + 1]) {
-                dp[i][i + 1] = true;
-                palindrome = s.substr(i, 2);
+            // 去重
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int target = 0 - nums[i], l = i + 1, r = size - 1;
+
+            while (l < r) {
+                if (nums[l] + nums[r] == target) {
+                    res.push_back({nums[i], nums[l], nums[r]});
+                    // 左右都要去重
+                    while (l < r && nums[l] == nums[l + 1])
+                        ++l;
+                    while (l < r && nums[r] == nums[r - 1])
+                        --r;
+                    ++l;
+                    --r;
+                } else if (nums[l] + nums[r] < target)
+                    ++l;
+                else
+                    --r;
             }
         }
 
-        if (size == 2)
-            return palindrome;
-
-        // if length > 2
-        int j = 0;
-        for (int k = 2; k < size; k++) {
-            for (int i = 0; i < size; i++) {
-                j = k + i;
-                if (j < size && s[i] == s[j] && dp[i + 1][j - 1]) {
-                    int len = j - i + 1;
-                    if (len >= maxLen) {
-                        maxLen = len;
-                        palindrome = s.substr(i, len);
-                    }
-                    dp[i][j] = true;
-                }
-            }
-        }
-        return palindrome;
+        return res;
     }
 };
 
 int main() {
-    string s = "ababababa";
-    cout << Solution().longestPalindrome(s) << endl;
+    vector<int> nums = {0, 0, 0, 0};
+    vector<vector<int>> res = Solution().threeSum(nums);
+    for (int i = 0; i < res.size(); i++) {
+        for (int j = 0; j < res[0].size(); j++) {
+            cout << res[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
--- solution0015_3Sum/main.cpp
+++ solution0015_3Sum/main.cpp
@@ -17,7 +17,6 @@
 
 using namespace std;
 
-// TODO
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int> &nums) {
@@ -25,22 +24,51 @@ public:
         int size = nums.size();
         if (size <= 2)
             return res;
-        unordered_map<int, bool> record;
-        for (int i = 0; i < size; i++) {
-            record[nums[i]] = true;
-        }
-        for (int i = 0; i < size; i++) {
-            for (int j = i + 1; j < size; j++) {
 
+        sort(nums.begin(), nums.end());
+        if (nums[0] > 0 || nums[size - 1] < 0)
+            return res;
+
+        for (int i = 0; i < size - 2; i++) {
+            // 剪枝, 因为此时array已经sort过了,
+            // 如果nums[i]都已经非负了, 一定不可能3个数和为0
+            if (nums[i] > 0)
+                break;
+
+            // 去重
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int target = 0 - nums[i], l = i + 1, r = size - 1;
+
+            while (l < r) {
+                if (nums[l] + nums[r] == target) {
+                    res.push_back({nums[i], nums[l], nums[r]});
+                    // 左右都要去重
+                    while (l < r && nums[l] == nums[l + 1])
+                        ++l;
+                    while (l < r && nums[r] == nums[r - 1])
+                        --r;
+                    ++l;
+                    --r;
+                } else if (nums[l] + nums[r] < target)
+                    ++l;
+                else
+                    --r;
             }
         }
+
+        return res;
     }
 };
 
 int main() {
-    vector<int> nums = {1, 2, 0};
-    // {1, 2, 0};
-    // {3, 4, -1, -2, 1, 5, 16, 0, 2, 0};
-
-    cout << Solution().firstMissingPositive(nums) << endl;
+    vector<int> nums = {0, 0, 0, 0};
+    vector<vector<int>> res = Solution().threeSum(nums);
+    for (int i = 0; i < res.size(); i++) {
+        for (int j = 0; j < res[0].size(); j++) {
+            cout << res[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
