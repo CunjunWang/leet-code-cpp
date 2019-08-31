a389cbf672c12d1787d58ac1d6237da8527ba018
Cunjun Wang <13621691063@163.com>
2019-08-31 10:17:16 -0400

    270 finishes

---
+++ solution0222_CountCompleteTreeNodes/main.cpp
@@ -0,0 +1,43 @@
+//
+// Created by 王存俊 on 2019-08-31.
+//
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+#include <sstream>
+#include <cassert>
+#include <list>
+#include <set>
+#include <unordered_set>
+#include "TreeNode.h"
+#include "ListNode.h"
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ // TODO: do with "complete"
+class Solution {
+
+public:
+    int countNodes(TreeNode *root) {
+        if (!root)
+            return 0;
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
+
+int main() {
+}
