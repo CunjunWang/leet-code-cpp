c2f199f84bf2b25449dce97562b17100f2bf1a6c
Cunjun Wang <13621691063@163.com>
2019-09-03 13:35:03 -0400

    958 finishes

--- main.cpp
+++ main.cpp
@@ -15,97 +15,52 @@
 
 using namespace std;
 
-class TrieNode {
-public:
-    int index; // 当前单词在words中的index. 如果不是单词则为-1
-    unordered_map<char, TrieNode *> next;
-    vector<int> list; // 记录符合如下条件的index: 1. words[index]以当前node为后缀 2. words[index]剩余部分为palindrome
-
-    TrieNode() {
-        index = -1;
-    }
-};
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
-private:
-    // 所有单词倒序添加进trie
-    // 如果是单词, 记录该单词在word list中的index, 否则是-1
-    void addWord(TrieNode *node, string word, int index) {
-        for (int i = word.size() - 1; i >= 0; i--) {
-            char c = word[i];
-            if (!node->next.count(c)) {
-                node->next[c] = new TrieNode();
-            }
-            if (isPalindrome(word, 0, i)) {
-                node->list.emplace_back(index);
-            }
-            node = node->next[c];
-        }
-        node->list.emplace_back(index);
-        node->index = index;
-    }
-
-    // 在trie中搜索
-    void searchWord(TrieNode *node, int index, vector<string> &words,
-                    vector<vector<int>> &res) {
-        string thisWord = words[index];
-        for (int i = 0; i < thisWord.size(); i++) {
-            char c = thisWord[i];
-            if (node->index >= 0 && node->index != index &&
-                isPalindrome(thisWord, i, thisWord.size() - 1)) {
-                vector<int> candidate = {index, node->index};
-                res.emplace_back(candidate);
-            }
-            node = node->next[c];
-            if (!node) {
-                return;
-            }
-        }
-
-        for (int i : node->list) {
-            if (index != i) {
-                vector<int> candidate = {index, i};
-                res.emplace_back(candidate);
-            }
-        }
-    }
-
-    // 判断str从start到end是不是palindrome
-    bool isPalindrome(string &str, int start, int end) {
-        while (start <= end) {
-            if (str[start] != str[end])
-                return false;
-            else
-                start++, end--;
-        }
-        return true;
-    }
-
 public:
-    vector<vector<int>> palindromePairs(vector<string> &words) {
-        vector<vector<int>> res;
-        int size = words.size();
-        if (size <= 1)
-            return res;
+    bool isCompleteTree(TreeNode *root) {
+        if (!root)
+            return true;
 
-        TrieNode *root = new TrieNode();
-        // 把每个单词以倒序加进trie
-        for (int i = 0; i < size; i++) {
-            addWord(root, words[i], i);
+        vector<TreeNode *> todo;
+        todo.emplace_back(root);
+        int i = 0;
+        while (i < todo.size()) {
+            TreeNode *cur = todo[i];
+            if (cur) {
+                todo.emplace_back(cur->left);
+                todo.emplace_back(cur->right);
+            } else {
+                break;
+            }
+            i++;
         }
-        // 对每个单词去trie中搜索
-        for (int i = 0; i < size; i++) {
-            searchWord(root, i, words, res);
+        cout << todo.size() << endl;
+        while (i < todo.size() && !todo[i]) {
+            i++;
         }
 
-        return res;
+        return i == todo.size();
     }
 };
 
 int main() {
-    vector<string> words = {"a", "abc", "aba", ""};
-    // {"a", ""};
-    // {"abcd", "dcba", "lls", "s", "sssll"};
-    vector<vector<int>> res = Solution().palindromePairs(words);
-    printMatrix(res);
+    TreeNode *root = new TreeNode(1);
+    TreeNode *node1 = new TreeNode(2);
+    TreeNode *node2 = new TreeNode(3);
+    TreeNode *node3 = new TreeNode(4);
+    TreeNode *node4 = new TreeNode(5);
+    TreeNode *node5 = new TreeNode(6);
+    root->left = node1, root->right = node2;
+    node1->left = node3;
+    node2->left = node4, node2->right = node5;
+    cout << Solution().isCompleteTree(root) << endl;
 }
--- solution0958_CheckCompletenessOfABinaryTree/main.cpp
+++ solution0958_CheckCompletenessOfABinaryTree/main.cpp
@@ -1,6 +1,22 @@
 //
 // Created by 王存俊 on 2019-09-02.
 //
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+#include <sstream>
+#include <cassert>
+#include <list>
+#include <set>
+#include <unordered_set>
+#include "TreeNode.h"
+#include "ListNode.h"
+#include "Helpers.h"
+
+using namespace std;
 
 /**
  * Definition for a binary tree node.
@@ -13,7 +29,41 @@
  */
 class Solution {
 public:
-    bool isCompleteTree(TreeNode* root) {
+    bool isCompleteTree(TreeNode *root) {
+        if (!root)
+            return true;
 
+        vector<TreeNode *> todo;
+        todo.emplace_back(root);
+        int i = 0;
+        while (i < todo.size()) {
+            TreeNode *cur = todo[i];
+            if (cur) {
+                todo.emplace_back(cur->left);
+                todo.emplace_back(cur->right);
+            } else {
+                break;
+            }
+            i++;
+        }
+        cout << todo.size() << endl;
+        while (i < todo.size() && !todo[i]) {
+            i++;
+        }
+
+        return i == todo.size();
     }
 };
+
+int main() {
+    TreeNode *root = new TreeNode(1);
+    TreeNode *node1 = new TreeNode(2);
+    TreeNode *node2 = new TreeNode(3);
+    TreeNode *node3 = new TreeNode(4);
+    TreeNode *node4 = new TreeNode(5);
+    TreeNode *node5 = new TreeNode(6);
+    root->left = node1, root->right = node2;
+    node1->left = node3;
+    node2->left = node4, node2->right = node5;
+    cout << Solution().isCompleteTree(root) << endl;
+}
