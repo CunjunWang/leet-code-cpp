952926d7766b37b00cf940f7018255d3d96c19ed
Cunjun Wang <13621691063@163.com>
2019-09-03 13:09:45 -0400

    336 finishes

--- main.cpp
+++ main.cpp
@@ -15,38 +15,97 @@
 
 using namespace std;
 
-class Solution {
+class TrieNode {
 public:
-    bool search(vector<int> &nums, int target) {
-        int size = nums.size();
-        if (size == 0)
-            return false;
-        // binary search
-        int left = 0, right = size - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                return true;
-            }
+    int index; // 当前单词在words中的index. 如果不是单词则为-1
+    unordered_map<char, TrieNode *> next;
+    vector<int> list; // 记录符合如下条件的index: 1. words[index]以当前node为后缀 2. words[index]剩余部分为palindrome
 
-            if (nums[left] == nums[mid] && nums[mid] == nums[right])
-                left++, right--;
-            else if (nums[mid] < nums[left]) {
-                if (target > nums[mid] && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            } else {
-                if (target < nums[mid] && target >= nums[left]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            }
-        }
-        return false;
+    TrieNode() {
+        index = -1;
     }
 };
 
-int main() {}
+class Solution {
+private:
+    // 所有单词倒序添加进trie
+    // 如果是单词, 记录该单词在word list中的index, 否则是-1
+    void addWord(TrieNode *node, string word, int index) {
+        for (int i = word.size() - 1; i >= 0; i--) {
+            char c = word[i];
+            if (!node->next.count(c)) {
+                node->next[c] = new TrieNode();
+            }
+            if (isPalindrome(word, 0, i)) {
+                node->list.emplace_back(index);
+            }
+            node = node->next[c];
+        }
+        node->list.emplace_back(index);
+        node->index = index;
+    }
+
+    // 在trie中搜索
+    void searchWord(TrieNode *node, int index, vector<string> &words,
+                    vector<vector<int>> &res) {
+        string thisWord = words[index];
+        for (int i = 0; i < thisWord.size(); i++) {
+            char c = thisWord[i];
+            if (node->index >= 0 && node->index != index &&
+                isPalindrome(thisWord, i, thisWord.size() - 1)) {
+                vector<int> candidate = {index, node->index};
+                res.emplace_back(candidate);
+            }
+            node = node->next[c];
+            if (!node) {
+                return;
+            }
+        }
+
+        for (int i : node->list) {
+            if (index != i) {
+                vector<int> candidate = {index, i};
+                res.emplace_back(candidate);
+            }
+        }
+    }
+
+    // 判断str从start到end是不是palindrome
+    bool isPalindrome(string &str, int start, int end) {
+        while (start <= end) {
+            if (str[start] != str[end])
+                return false;
+            else
+                start++, end--;
+        }
+        return true;
+    }
+
+public:
+    vector<vector<int>> palindromePairs(vector<string> &words) {
+        vector<vector<int>> res;
+        int size = words.size();
+        if (size <= 1)
+            return res;
+
+        TrieNode *root = new TrieNode();
+        // 把每个单词以倒序加进trie
+        for (int i = 0; i < size; i++) {
+            addWord(root, words[i], i);
+        }
+        // 对每个单词去trie中搜索
+        for (int i = 0; i < size; i++) {
+            searchWord(root, i, words, res);
+        }
+
+        return res;
+    }
+};
+
+int main() {
+    vector<string> words = {"a", "abc", "aba", ""};
+    // {"a", ""};
+    // {"abcd", "dcba", "lls", "s", "sssll"};
+    vector<vector<int>> res = Solution().palindromePairs(words);
+    printMatrix(res);
+}
--- solution0336_PalindromePairs/main.cpp
+++ solution0336_PalindromePairs/main.cpp
@@ -19,7 +19,117 @@
 
 using namespace std;
 
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+#include <sstream>
+#include <cassert>
+#include <list>
+#include <set>
+#include <unordered_set>
+#include "TreeNode.h"
+#include "ListNode.h"
+#include "Helpers.h"
 
+using namespace std;
+
+class TrieNode {
+public:
+    int index; // 当前单词在words中的index. 如果不是单词则为-1
+    unordered_map<char, TrieNode *> next;
+    vector<int> list; // 记录符合如下条件的index: 1. words[index]以当前node为后缀 2. words[index]剩余部分为palindrome
+
+    TrieNode() {
+        index = -1;
+    }
+};
+
+class Solution {
+private:
+    // 所有单词倒序添加进trie
+    // 如果是单词, 记录该单词在word list中的index, 否则是-1
+    void addWord(TrieNode *node, string word, int index) {
+        for (int i = word.size() - 1; i >= 0; i--) {
+            char c = word[i];
+            if (!node->next.count(c)) {
+                node->next[c] = new TrieNode();
+            }
+            if (isPalindrome(word, 0, i)) {
+                node->list.emplace_back(index);
+            }
+            node = node->next[c];
+        }
+        node->list.emplace_back(index);
+        node->index = index;
+    }
+
+    // 在trie中搜索
+    void searchWord(TrieNode *node, int index, vector<string> &words,
+                    vector<vector<int>> &res) {
+        string thisWord = words[index];
+        for (int i = 0; i < thisWord.size(); i++) {
+            char c = thisWord[i];
+            if (node->index >= 0 && node->index != index &&
+                isPalindrome(thisWord, i, thisWord.size() - 1)) {
+                vector<int> candidate = {index, node->index};
+                res.emplace_back(candidate);
+            }
+            node = node->next[c];
+            if (!node) {
+                return;
+            }
+        }
+
+        for (int i : node->list) {
+            if (index != i) {
+                vector<int> candidate = {index, i};
+                res.emplace_back(candidate);
+            }
+        }
+    }
+
+    // 判断str从start到end是不是palindrome
+    bool isPalindrome(string &str, int start, int end) {
+        while (start <= end) {
+            if (str[start] != str[end])
+                return false;
+            else
+                start++, end--;
+        }
+        return true;
+    }
+
+public:
+    vector<vector<int>> palindromePairs(vector<string> &words) {
+        vector<vector<int>> res;
+        int size = words.size();
+        if (size <= 1)
+            return res;
+
+        TrieNode *root = new TrieNode();
+        // 把每个单词以倒序加进trie
+        for (int i = 0; i < size; i++) {
+            addWord(root, words[i], i);
+        }
+        // 对每个单词去trie中搜索
+        for (int i = 0; i < size; i++) {
+            searchWord(root, i, words, res);
+        }
+
+        return res;
+    }
+};
+
+int main() {
+    vector<string> words = {"a", "abc", "aba", ""};
+    // {"a", ""};
+    // {"abcd", "dcba", "lls", "s", "sssll"};
+    vector<vector<int>> res = Solution().palindromePairs(words);
+    printMatrix(res);
+}
 
 // brute force solution, TLE
 //class Solution {
@@ -58,9 +168,3 @@ using namespace std;
 //        return res;
 //    }
 //};
-
-int main() {
-    vector<string> words = {"abcd", "dcba", "lls", "s", "sssll"};
-    vector<vector<int>> res = Solution().palindromePairs(words);
-    printMatrix(res);
-}
