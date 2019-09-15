3dd8dc24ee34281f22fe7b331c2ac4ceef3644ca
Cunjun Wang <duckwcj@gmail.com>
2019-09-15 10:16:21 -0400

    739 finishes

---
+++ solution0438_FindAllAnagramsInAString/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 王存俊 on 2019-09-14.
+//
+
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+
+    }
+};
---
+++ solution0706_DesignHashMap/main.cpp
@@ -0,0 +1,35 @@
+//
+// Created by 王存俊 on 2019-09-15.
+//
+
+// TODO: see the video
+class MyHashMap {
+public:
+    /** Initialize your data structure here. */
+    MyHashMap() {
+
+    }
+
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+
+    }
+
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+
+    }
+
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
