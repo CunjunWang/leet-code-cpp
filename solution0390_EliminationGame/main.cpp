a04fb8ab764a937f07414ddfda38f893f1c417eb
Cunjun Wang <duckwcj@gmail.com>
2019-09-27 18:02:12 -0400

    116 finishes

---
+++ solution0390_EliminationGame/main.cpp
@@ -0,0 +1,33 @@
+//
+// Created by 王存俊 on 2019/9/27.
+//
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+#include <sstream>
+#include <cassert>
+#include <list>
+#include <set>
+#include <unordered_set>
+#include "TreeNode.h"
+#include "Helpers.h"
+
+using namespace std;
+
+// TODO:
+class Solution {
+public:
+	int lastRemaining(int n) {
+
+	}
+};
+
+int main() {
+	string str = "bb";
+	vector<vector<string>> res = Solution().flatten(str);
+	printMatrix(res);
+}
